870201cbd287875be9782341cdce5a90f94a2586
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2024-04-13 22:38:03 +0200

    Added Win32 Exception class

---
+++ include/Junia/Exceptions/ExWin32.hpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *
+ * @file      ExWin32.hpp
+ * @brief     Contains the ExWin32 exception class definition
+ * @author    Max Hager
+ * @date      13.04.2024
+ * @copyright © Max Hager, 2024. All right reserved.
+ *
+ ******************************************************************************/
+
+#ifndef __HEADER_JUNIA_EXCEPTIONS_EXWIN32
+#define __HEADER_JUNIA_EXCEPTIONS_EXWIN32
+
+#include "../Core/Core.hpp"
+
+#include "../Core/Exception.hpp"
+
+namespace Junia {
+
+class JUNIA_SYMBOL ExWin32 : public Exception {
+public:
+	/**
+	 * @brief ExWin32 object constructor. Will set the message to
+	 *        "Win32 Error Code <code>" if the message text could not be
+	 *        retrieved from the system.
+	 * @param code     the exception code of the message
+	 * @param previous an exception that led to this exception or a nullptr
+	 * @param location the code position this exception was thrown in (see
+	 *                 JUNIA_CODEPOS)
+	 */
+	ExWin32(unsigned long code, std::exception_ptr previous, CodePos location) noexcept;
+
+protected:
+	unsigned long code;
+};
+
+} // namespace Junia
+
+#endif // !defined(__HEADER_JUNIA_EXCEPTIONS_EXWIN32)
---
+++ src/Junia/Exceptions/ExWin32.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *
+ * @file      ExWin32.cpp
+ * @brief     Contains the ExWin32 exception class implementation
+ * @author    Max Hager
+ * @date      13.04.2024
+ * @copyright © Max Hager, 2024. All right reserved.
+ *
+ ******************************************************************************/
+
+#include <Junia/Exceptions/ExWin32.hpp>
+
+#include <Junia/Core/StringConvert.hpp>
+
+#define _WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#undef _WIN32_LEAN_AND_MEAN
+
+namespace Junia {
+
+ExWin32::ExWin32(unsigned long code, std::exception_ptr previous, CodePos location) noexcept
+	: code(code), Exception(u8"", previous, location) {
+	LPWSTR buf;
+	DWORD  strLen = FormatMessageW(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL,
+        code,
+        LANG_USER_DEFAULT,
+        reinterpret_cast<LPWSTR>(&buf),
+        1024,
+        NULL);
+
+	if (strLen == 0) {
+		std::string codeStr = std::to_string(code);
+		Exception::message  = u8"Win32 Error Code "; // short string
+		// should stay a short string because error codes only go up to 15999
+		for (std::size_t i = 0; i < codeStr.size(); i++) Exception::message += codeStr[i];
+		return;
+	}
+
+	Exception::message = StringConvert::U16ToU8(buf);
+	LocalFree(buf);
+}
+
+} // namespace Junia
